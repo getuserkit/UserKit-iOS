973de5f8dfd13380ad363f9635199e68812ba7fc
Peter Nicholls <peterwnicholls@gmail.com>
2025-11-02 11:15:57 +1100

    Add mic toggle button to options

--- Sources/UserKit/Call/Call.swift
+++ Sources/UserKit/Call/Call.swift
@@ -552,6 +552,12 @@ extension Call: PictureInPictureViewControllerDelegate {
         }
                 
         await presentAlert(title: "Continue Call", message: "You are in a call with Peter", options: [
+            UIAlertAction(title: user.isMicrophoneEnabled ? "Mute Mic" : "Enable Mic", style: .default) { [weak self] alertAction in
+                Task {
+                    try await self?.user.setMicrophone(enabled: (self?.user.isMicrophoneEnabled ?? true) ? false : true)
+                    await self?.startPictureInPicture()
+                }
+            },
             UIAlertAction(title: user.isCameraEnabled ? "Disable Camera" : "Enable Camera", style: .default) { [weak self] alertAction in
                 Task {
                     try await self?.user.setCamera(enabled: (self?.user.isCameraEnabled ?? true) ? false : true)
